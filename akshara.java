40755702f34b11706bbfe3fc3cddec6e35a8cfa8
root <root@git-server.example.com>
2024-04-24 05:38:00 +0000

    projectmilestone2

---
+++ akshara.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+        // The System.out.println method prints a line of text to the console
+        System.out.println("Hello World");
+    }
+}
